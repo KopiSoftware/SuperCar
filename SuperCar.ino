81291b245eaa9f25354ca4e14faeb089ae1d681d
KopiSoftware <530579714@qq.com>
2023-08-02 14:48:25 +0800

    pwm fixed; cmd added

--- SuperCar.ino
+++ SuperCar.ino
@@ -1,25 +1,41 @@
-#include <Arduino.h>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+
+
 #define AIN1 26
 #define AIN2 27
 #define BIN1 17
 #define BIN2 5
 
+#define PWM1  0
+#define PWM2  1
+#define PWM3  2
+#define PWM4  3
+#define freq 50000      //PWM波形频率5KHZ
+#define resolution  10    //使用PWM占空比的分辨率，占空比最大可写2^10-1=1023
+
 #define RA  12
 #define RB  14
 #define LA  18
 #define LB  19
+
 #define MOVING_PULSE 100
 
 long cnt_L, cnt_R = 0;
 int Velocity_Left, Velocity_Right = 0;
+int pwm_l = 0, pwm_r=0; //Motor pwm speed
 hw_timer_t *timer = NULL;
 
 void Left_encoder_isr_A();
 void Left_encoder_isr_B();
-void front(int ratio);
-void left (int ratio);
-void right(int ratio);
-void back (int ratio);
+void front(int pwm_v);
+void left (int pwm_v);
+void right(int pwm_v);
+void back (int pwm_v);
 void sstop();
 //void IRAM_ATTR TimerEvent();
 void IRAM_ATTR update_speed();
@@ -34,41 +50,76 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(RB), Left_encoder_isr_A, CHANGE);
   attachInterrupt(digitalPinToInterrupt(LB), Left_encoder_isr_B, CHANGE);
 
+  // PWM settings  
+  // Wheel 1 forward
+  ledcSetup(PWM1, freq, resolution);  //PWM通道一开启设置
+  ledcAttachPin(AIN1, PWM1);     //PWM通道一和引脚PWMA关联
+  ledcWrite(PWM1, 0);        //PWM通道一占空比设置为零
+  // Wheel 1 backward
+  ledcSetup(PWM2, freq, resolution);
+  ledcAttachPin(AIN2, PWM2); 
+  ledcWrite(PWM2, 0); 
+
+  // PWM settings  
+  // Wheel 2 forward
+  ledcSetup(PWM3, freq, resolution);
+  ledcAttachPin(BIN1, PWM3); 
+  ledcWrite(PWM3, 0); 
+  // Wheel 2 backward
+  ledcSetup(PWM4, freq, resolution);
+  ledcAttachPin(BIN2, PWM4);
+  ledcWrite(PWM4, 0);
+  
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &update_speed, true);
   timerAlarmWrite(timer, 100000, true); //100ms update
   timerAlarmEnable(timer);
 }
 
-void test_vot(float vot){
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, vot);
-  digitalWrite(BIN1, vot);
-  digitalWrite(BIN2, LOW);
-  delay(10);
-}
-
-float vot = 5.0;
-
+int pwm_v = 700;
+char cmd = 's';
 void loop() {
-//  Serial.print("R: ");
-//  Serial.print(cnt_R);
-//  Serial.print(" L: ");
-//  Serial.println(cnt_L);
-  if(vot>1.0)
-    vot-=0.2;
-  else
-    vot = 5.0;
-  test_vot(vot);
+    
   Serial.print("Voltage: ");
-  Serial.print(vot);
+  Serial.print(pwm_v);
   Serial.print(" VR: ");
   Serial.print(Velocity_Left);
   Serial.print(" VL: ");
   Serial.println(Velocity_Right);
-  delay(500);
 
-//  front(10);
+  if(Serial.available())
+    cmd = (char)Serial.read();
+
+  Serial.println(cmd);
+  switch(cmd){
+    case 'w': 
+      Serial.print("front");
+      front(pwm_v);
+      break;
+    case 'a':
+      Serial.print("front");
+      left(pwm_v);
+      break;
+    case 'd':
+      right(pwm_v);
+      break;
+    case 'x':
+      back(pwm_v);
+      break;
+    case 's':
+      sstop();
+      break;
+    case '+':
+      if(pwm_v+100<1000) pwm_v+=20;
+      break;
+    case '-':
+      if(pwm_v-600>0   ) pwm_v-=20;
+      break;
+    default:
+      ;
+  }
+  cmd = 's';
+
 }
 
 void IRAM_ATTR update_speed(){
@@ -79,85 +130,51 @@ void IRAM_ATTR update_speed(){
 
 }
 
-void IRAM_ATTR TimerEvent()
-{
-    Serial.println("Timer called");
-}
-
 void Left_encoder_isr_A(){
   if (digitalRead(RA)!=digitalRead(RB)) cnt_L++;
   else cnt_L--;
 }
 
 void Left_encoder_isr_B(){
-  if (digitalRead(LA)!=digitalRead(LB)) cnt_R++;
-  else  cnt_R--; 
+  if (digitalRead(LA)!=digitalRead(LB)) cnt_R--;
+  else  cnt_R++; 
 }
 
-void front(int ratio){
-  Serial.println("FRONT");
-  if(ratio > 100) ratio%=100;
-  int empty = MOVING_PULSE - ratio;
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
-  digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
-  delay(ratio);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
-  delay(empty);
+void front(int pwm_v){
+    ledcWrite(PWM1, 0);
+    ledcWrite(PWM2, pwm_v);
+    ledcWrite(PWM3, pwm_v);
+    ledcWrite(PWM4, 0);
+    delay(20);
 }
 
-void left(int ratio){
-  Serial.println("LEFT");
-  if(ratio > 100) ratio%=100;
-  int empty = MOVING_PULSE - ratio;
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, HIGH);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, HIGH);
-  delay(ratio);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
-  delay(empty);
+void back(int pwm_v){
+    ledcWrite(PWM1, pwm_v);
+    ledcWrite(PWM3, 0);
+    ledcWrite(PWM2, 0);
+    ledcWrite(PWM4, pwm_v);
+    delay(20);
 }
-void right(int ratio){
-  Serial.println("RIGHT");
-  if(ratio > 100) ratio%=100;
-  int empty = MOVING_PULSE - ratio;
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, HIGH);
-  digitalWrite(BIN2, LOW);
-  delay(ratio);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
-  delay(empty);
+
+void left(int pwm_v){
+    ledcWrite(PWM1, 0);
+    ledcWrite(PWM2, pwm_v); //rigth forward
+    ledcWrite(PWM3, 0);
+    ledcWrite(PWM4, pwm_v);
+    delay(20);
 }
-void back(int ratio){
-  Serial.println("BACK");
-  if(ratio > 100) ratio%=100;
-  int empty = MOVING_PULSE - ratio;
-  digitalWrite(AIN1, HIGH);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, HIGH);
-  delay(ratio);
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
-  delay(empty);
+
+void right(int pwm_v){
+    ledcWrite(PWM1, pwm_v);
+    ledcWrite(PWM2, 0);
+    ledcWrite(PWM3, pwm_v);
+    ledcWrite(PWM4, 0);
+    delay(20);
 }
+
 void sstop(){
-  digitalWrite(AIN1, LOW);
-  digitalWrite(AIN2, LOW);
-  digitalWrite(BIN1, LOW);
-  digitalWrite(BIN2, LOW);
+    ledcWrite(PWM1, 0);
+    ledcWrite(PWM2, 0);
+    ledcWrite(PWM3, 0);
+    ledcWrite(PWM4, 0);
 }
